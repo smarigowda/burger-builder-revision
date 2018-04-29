bec53ef9ae8df16de12cd75cbd3698f4b0effc78
Santosh Marigowda <santosharakere@gmail.com>
2018-04-29 21:33:31 +0100

    BUR-3 component BurgerBuilder

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
+import classes from './Layout.css';
 
 const layout = props => {
   return (
     <Aux>
       <div>Toolbar, Side Drawer, Backdrop</div>
-      <main>
+      <main className={classes.Content}>
         {props.children}
       </main>
     </Aux>
