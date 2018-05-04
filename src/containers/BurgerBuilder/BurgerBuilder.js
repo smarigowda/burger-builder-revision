5cfb5c0f21c98c0d8addfc95045f89b24da3679c
Santosh Marigowda <santosharakere@gmail.com>
2018-05-04 22:32:01 +0100

    BUR-6 modal and order summary

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENTS_PRICE = {
   salad: 0.3,
@@ -79,6 +81,9 @@ class BurgerBuilder extends Component {
     }
     return (
        <Aux>
+         <Modal>
+          <OrderSummary ingredients={this.state.ingredients} />
+         </Modal>
          <Burger ingredients={this.state.ingredients}/>
          <BuildControls
             price={this.state.totalPrice}
