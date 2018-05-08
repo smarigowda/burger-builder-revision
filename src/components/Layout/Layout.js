4935627db18839f162f0d2694d3fe3312df86323
Santosh Marigowda <santosharakere@gmail.com>
2018-05-08 21:57:24 +0100

    BUR-11 NavigationItem(s) components

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
-import Toolbar from '../Navigation/Toolbar/Toolbar'
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = props => {
   return (
