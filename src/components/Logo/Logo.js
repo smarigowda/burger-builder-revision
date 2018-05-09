e9ce502cd3aedb2f8c444682acdff465eb2cc015
Santosh Marigowda <santosharakere@gmail.com>
2018-05-09 06:08:18 +0100

    BUR-11 using inline styles to control logo height

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,7 +3,11 @@ import burgerLogo from '../../assets/images/logo.png';
 import classes from './Logo.css';
 
 const logo = props => (
-  <div className={classes.Logo}>
+  <div className={classes.Logo} style={
+    {
+      height: props.height
+    }
+  }>
     <img src={burgerLogo} alt="My Burger"/>
   </div>
 )
