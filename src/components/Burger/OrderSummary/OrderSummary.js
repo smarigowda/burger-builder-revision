970a3be074d81131e8f50145af8173faf1fe490a
Santosh Marigowda <santosharakere@gmail.com>
2018-05-13 05:54:42 +0100

    BUR-12 render OrderSummary only when required

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = props => {
-  const list = Object.keys(props.ingredients)
-                     .map(key => <li key={key}><span style={{textTransform: 'capitalize'}}>{key}</span>: {props.ingredients[key]}</li>)
-  return (
-    <Aux>
-      <p><strong>Your delicious order with following ingredients:</strong></p>
-      <ul>
-        {list}
-      </ul>
-      <p><strong>Total Price: {props.totalPrice.toFixed(2)} $</strong></p>
-      <p>Continue to Checkout ?</p>
-      <Button btnType="Success" onClickHandler={() => { props.purchaseContinueHandler(); } }>CONTINUE</Button>
-      <Button btnType="Danger" onClickHandler={props.purchaseCancelHandler} >CANCEL</Button>
-    </Aux>
-  );
-};
+class OrderSummary extends Component {
+  
+  componentWillUpdate = () => {
+    console.log('[OrderSummary] componentWillUpdate...');
+  }
+  render() {
+    const list = Object.keys(this.props.ingredients)
+                       .map(key => <li key={key}><span style={{textTransform: 'capitalize'}}>{key}</span>: {this.props.ingredients[key]}</li>)
+    return (
+      <Aux>
+        <p><strong>Your delicious order with following ingredients:</strong></p>
+        <ul>
+          {list}
+        </ul>
+        <p><strong>Total Price: {this.props.totalPrice.toFixed(2)} $</strong></p>
+        <p>Continue to Checkout ?</p>
+        <Button btnType="Success" onClickHandler={() => { this.props.purchaseContinueHandler(); } }>CONTINUE</Button>
+        <Button btnType="Danger" onClickHandler={this.props.purchaseCancelHandler} >CANCEL</Button>
+      </Aux>
+    );
+  }
+}
 
-export default orderSummary;
+export default OrderSummary;
