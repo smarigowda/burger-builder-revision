9ee93a6e42e0120b390bdeeacc2d0a964ba97797
Santosh Marigowda <santosharakere@gmail.com>
2018-06-03 19:10:33 +0100

    BUR-21 nav links completed

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -7,7 +7,9 @@ const navigationItem = props => {
   return (
     <li className={classes.NavigationItem}>
       <NavLink
-        to={props.link}>{props.children}</NavLink>
+        to={props.link}
+        activeClassName={classes.active}
+        exact={props.exact}>{props.children}</NavLink>
     </li>
   )
 }
